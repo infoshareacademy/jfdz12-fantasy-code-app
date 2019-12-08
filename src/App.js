c33657bf9939ad83dd4efa34508276344f4624f9
piotr-andrzej-bochenek <piotr8207@interia.eu>
2019-12-08 18:13:25 +0100

    initial commit dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import {Home} from './menu/home/index'
 
 function App() {
   return (
     <div >
-      
+      <Home>
+        
+      </Home>
     </div>
   );
 }
