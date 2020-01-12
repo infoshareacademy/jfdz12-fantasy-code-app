c999e5d5887640c1147dcee606e2bf19f11f4431
Karol Nogal <karolnogal92@gmail.com>
2020-01-12 14:44:14 +0100

    game details modal added , ui components added , loader added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div >
       <GameCard/>
-      <Home />
+      <Home/>
 
     </div>
   );
--- src/menu/games/index.js
+++ src/menu/games/index.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { Button, Card } from 'semantic-ui-react'
-
- export class GameCard extends React.Component{
+import { Button, Card, Divider, Dimmer, Loader, Image, Segment  } from 'semantic-ui-react'
+import {GameDetails} from "./game-details/index"
+export class GameCard extends React.Component{
     constructor(props){
         super(props)
         this.state={
             games:[],
-            loading: false,
+            loading: true,
             error: null,
-            expanded: false
         }
     }
     componentDidMount(){
@@ -19,21 +18,22 @@ import { Button, Card } from 'semantic-ui-react'
             .then(resp=>resp.json())
             .then(resp=>
                this.setState({
-                    games: resp
+                    games: resp,
+                    loading:false
                })) 
             .catch(error=>this.setState({
                 error:"err occ"
             }))  
     }
-
     displayGameKind(){
             return(this.state.games.map(game=>
-    <Card>
+    <Card key={game.id}>
       <Card.Content>
-        <Card.Header>Steve Sanders</Card.Header>
-        <Card.Meta>Friends of Elliot</Card.Meta>
+        <Card.Header textAlign="center">{game.title}</Card.Header>
+        <Divider/>
+            <Card.Meta>{game.localization.city}</Card.Meta>
         <Card.Description>
-          {game.title}
+              
         </Card.Description>
       </Card.Content>
       <Card.Content extra>
@@ -41,30 +41,40 @@ import { Button, Card } from 'semantic-ui-react'
           <Button basic color='green'>
             Approve
           </Button>
-          <Button basic color='blue'>
-            Details
-          </Button>
+         <GameDetails 
+          title={game.title}
+          localization={game.localization.place}
+          date={game.date}
+          playerMax={game.palyer.max}  
+          playerCur={game.palyer.current}  
+          reqLvl={game.ReqLevelID}
+          descript={game.Description}
+         />
         </div>
       </Card.Content>
     </Card> 
      ))
     }
     render(){
+      if(this.state.loading){
         return(
+          <Segment>
+          <Dimmer active>
+            <Loader size='massive'>Loading</Loader>
+          </Dimmer>
+    
+          <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+          <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+          <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+        </Segment>
+        )
+      }else return(
+        
             <div>
                 <Card.Group margin="12px">
                   {this.displayGameKind()}
                 </Card.Group>
-                  
-                  
             </div>
         )
     }
 }
-
-
-
-
-
-
-
