246376dc8061fb376903f925f9c465bd350b0b1f
Maksymilian Tarnoruda <m.tarnoruda@gmail.com>
2020-01-19 20:03:10 +0100

    FZ12FC-56-Firebase config initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCZpkhY-UUq27S_GferQx06LjUSvoCHXrk",
+    authDomain: "fantasyapp-9473b.firebaseapp.com",
+    databaseURL: "https://fantasyapp-9473b.firebaseio.com",
+    projectId: "fantasyapp-9473b",
+    storageBucket: "fantasyapp-9473b.appspot.com",
+    messagingSenderId: "521438178228",
+    appId: "1:521438178228:web:3970e009265778728b60e1",
+    measurementId: "G-EP1ZJ1HTXV"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
