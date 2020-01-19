e493fb4822d6b6041ec5f35ad606cc594d611459
piotr-andrzej-bochenek <piotr8207@interia.eu>
2020-01-19 13:37:00 +0100

    FC-55 Functions initial RWD

--- src/menu/home/Functions.js
+++ src/menu/home/Functions.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 export default function Functions () {
     return (
-        <div className='Home__Functions'>
-            <div className='Home__Functions--title'>
-                <div>Choose game</div>
-                <div>Gather players</div>
-                <div>Fix place and date</div>
-                <div>Reserve table</div>
+        <div className='Home__Functions--container'>
+            <div className='Home__Functions--header'>
+                <div className='Home__Functions--header--1'>Choose game</div>
+                <div className='Home__Functions--header--2'>Gather players</div>
+                <div className='Home__Functions--header--1'>Fix place and date</div>
+                <div className='Home__Functions--header--2'>Reserve table</div>
             </div>
             <div className='Home__Functions--description'>
-                <div>Pick type and title</div>
-                <div>Pick number and level of players</div>
-                <div>Decide where and when to play</div>
-                <div>Book room for your game</div>
+                <div className='Home__Functions--description--1'>Pick type and title</div>
+                <div className='Home__Functions--description--2'>Pick number and level of players</div>
+                <div className='Home__Functions--description--1'>Decide where and when to play</div>
+                <div className='Home__Functions--description--2'>Book room for your game</div>
             </div>
         </div>
     );
