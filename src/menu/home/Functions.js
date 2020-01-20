fbf06c84234a899a71ceb00c1e4b67c3a1a43812
piotr-andrzej-bochenek <piotr8207@interia.eu>
2020-01-20 10:30:30 +0100

    FC-62 add RWD container to Home, restyle Welcome and Functions

--- src/menu/home/Functions.js
+++ src/menu/home/Functions.js
@@ -3,18 +3,25 @@ import React from 'react';
 export default function Functions () {
     return (
         <div className='Home__Functions--container'>
-            <div className='Home__Functions--header'>
-                <div className='Home__Functions--header--1'>Choose game</div>
-                <div className='Home__Functions--header--2'>Gather players</div>
-                <div className='Home__Functions--header--1'>Fix place and date</div>
-                <div className='Home__Functions--header--2'>Reserve table</div>
+            <div className='Home__Functions--items'>
+                <div>
+                    <p className='Home__Functions--header--1'>Choose game</p>
+                    <p className='Home__Functions--description--1'>Pick type and title</p>
+                </div>
+                <div>
+                    <p className='Home__Functions--header--2'>Gather players</p>
+                    <p className='Home__Functions--description--2'>Pick number and level of players</p>
+                </div>
+                <div>
+                    <p className='Home__Functions--header--1'>Fix place and date</p>
+                    <p className='Home__Functions--description--1'>Decide where and when to play</p>
+                </div>
+                <div>
+                    <p className='Home__Functions--header--2'>Reserve table</p>
+                    <p className='Home__Functions--description--2'>Book room for your game</p>
+                </div>
             </div>
-            <div className='Home__Functions--description'>
-                <div className='Home__Functions--description--1'>Pick type and title</div>
-                <div className='Home__Functions--description--2'>Pick number and level of players</div>
-                <div className='Home__Functions--description--1'>Decide where and when to play</div>
-                <div className='Home__Functions--description--2'>Book room for your game</div>
-            </div>
+            
         </div>
     );
 };
