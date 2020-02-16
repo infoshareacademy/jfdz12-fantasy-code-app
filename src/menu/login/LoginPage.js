277702501d1eeac07d9afa2851b9ecf61a35e2a5
Maksymilian Tarnoruda <m.tarnoruda@gmail.com>
2020-02-16 20:40:48 +0100

    FZ12FC-54-Login login with firebase

--- src/menu/login/LoginPage.js
+++ src/menu/login/LoginPage.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import firebase from 'firebase';
 import { Form, Segment } from 'semantic-ui-react';
 import { Link } from "react-router-dom";
+
+
+
 
 
 export class LoginPage extends React.Component {
@@ -9,25 +13,72 @@ export class LoginPage extends React.Component {
         this.state = {}
     }
 
+    handleChange = (e) => {
+        const value = e.target.value;
+        const name = e.target.name;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit = () => {
+        const email = this.state.email;
+        const password = this.state.password;
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+        });
+
+    }
+
+    componentDidMount() {
+        const authRef = firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                console.log(user)
+            } else {
+                console.log('not logged')
+            }
+        });
+
+        this.setState({
+            ref: authRef
+        })
+    }
+
+    componentWillUnmount() {
+        if(this.state.ref) {
+            this.state.ref();
+        }
+    }
+
     render() {
         return (
             <div>
-                <Form>
+                <Form onSubmit={this.handleSubmit}>
                     <Form.Input
                         // error={{ content: 'Please enter your first name', pointing: 'below' }}
                         fluid
-                        label='Nick'
-                        placeholder='Nick'
-                        id='form-input-nick'
+                        onChange={this.handleChange}
+                        label='Email'
+                        placeholder='Email'
+                        id='form-input-email'
+                        input='email'
+                        name='email'
                     />
                     <Form.Input
                         // error={{ content: 'Please enter your first name', pointing: 'below' }}
                         fluid
+                        onChange={this.handleChange}
                         label='Password'
                         placeholder='Password'
                         id='form-input-password'
+                        input='password'
+                        name='password'
                     />
-                    <Form.Button content='Login' />
+                    <Form.Button
+                        content='Login'
+                        type="submit"
+                    />
 
                     <Link to="/register">
                         Not registered yet?
