1c106d9f72c679311a068f76f6bd010fb370b395
karol-nogal <karolnogal92@gmail.com>
2020-02-20 20:43:36 +0100

    UI changes

--- src/menu/games/game-filter/GameFilter.js
+++ src/menu/games/game-filter/GameFilter.js
@@ -8,20 +8,20 @@ import "./filters.css"
 export default class GameFilter extends React.Component{
 render(){
     return(
-      <Segment  inverted color="blue">
-        <Form style={{display:"flex", alignItems:"center", justifyContent: "center"}}>
-          <Form.Group widths="equal">
+      <Segment className="ui grid centered"  inverted color="blue" style={{display:"flex", alignItems:"center", justifyContent: "center", paddingTop: "2%"}}>
+        <Form   >
+          <Form.Group  className="ui grid centered" >
             <Form.Input  label="Search by Name" placeholder="Game name" onChange={this.props.sortByName}/>
             <Form.Input   label="Search by City" placeholder="Location" onChange={this.props.sortByCity}/>
-            <SemanticDatepicker label="Search By Date"onChange={this.props.onChange}/>
-            <div className="radio">
-              <span className="radio-label">Hide Full games</span>
-              <Radio toggle onChange={this.props.onSlide}/>
+            <SemanticDatepicker  label="Search By Date"onChange={this.props.onChange}/>
+            <div className="ui grid">
+              <span className="radio-label centered row">Hide Full games</span>
+              <Radio className="centered column" style={{top: "-15px", left: "-15px"}} toggle onChange={this.props.onSlide}/>
             </div>
           </Form.Group>
           
         </Form>
-        <div className={"button_modal-centering"}>
+        <div className={"button_modal-centering "} >
         <NewGameFormModal  onAdd={this.props.onAdd}/>
         </div>
         
